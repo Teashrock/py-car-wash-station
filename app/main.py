66da2db5c3211c4aaf52d04e64baa0472ecba636
Teashrock <kajitsu22@gmail.com>
2026-05-29 22:10:57 +0300

    Flake8

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int,brand: str) -> None:
+    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -21,10 +21,10 @@ class CarWashStation:
 
     def calculate_washing_price(self, car: Car) -> float:
         return round(
-            car.comfort_class *
-            (self.clean_power - car.clean_mark) *
-            self.average_rating /
-            self.distance_from_city_center,
+            car.comfort_class
+            * (self.clean_power - car.clean_mark)
+            * self.average_rating
+            / self.distance_from_city_center,
             1
         )
 
